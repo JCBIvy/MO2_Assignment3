ba9fe8f9e4e4583bdbf057fdc0f524c64289f476
JCBIvy <157339653+JCBIvy@users.noreply.github.com>
2024-06-16 06:14:52 -0400

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,16 @@
+class Main
+{
+    //Establish constants
+
+    //INSERT METHOD DESCRIPTION HERE
+    public static String FUNCTION(int ARG)
+    {
+        return("Blah");
+    }
+
+    //Main body program
+    public static void main(String []args)
+    {
+        System.out.println("blah");
+    }
+};

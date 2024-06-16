2daf3a9ec5795d20be422c0e9b1b744cab89599b
JCBIvy <157339653+JCBIvy@users.noreply.github.com>
2024-06-16 12:49:11 +0000

    In progress

--- Main.java
+++ Main.java
@@ -2,15 +2,26 @@ class Main
 {
     //Establish constants
 
-    //INSERT METHOD DESCRIPTION HERE
-    public static String FUNCTION(int ARG)
+    //convert binary number to decimal number
+    public static int bin2Dec(String binaryString) throws BinaryFormatException
     {
-        return("Blah");
+        return(Integer.parseInt(binaryString, 2));
     }
 
     //Main body program
     public static void main(String []args)
     {
-        System.out.println("blah");
+        //make a binary string equal to 120 for easy testing
+        String binString = "1111000";
+        
+        //test normal conversion
+        System.out.println(bin2Dec(binString));
+
+        //change binString to something that'll cause an error
+        binString = "1111002";
+        
+        //test again with the bad string
+        System.out.println(bin2Dec(binString));
+
     }
 };
